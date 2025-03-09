96e075df29020c314f54c4b9696c955668c6d7c8
alenzyoth <adispt334@gmail.com>
2025-03-10 01:44:06 +0700

    second commit

--- Codelab/Codelab 1/src/Main.java
+++ Codelab/Codelab 1/src/Main.java
@@ -1,15 +1,47 @@
+import java.time.LocalDate;
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        public static void main(String[] args) {
+            // Membuat Scanner untuk membaca input dari pengguna
+            Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            // Meminta input nama
+            System.out.print("Masukkan nama: ");
+            String nama = scanner.nextLine();
+
+            // Meminta input jenis kelamin
+            System.out.print("Masukkan jenis kelamin (P/L): ");
+            char jenisKelamin = scanner.next().charAt(0);
+
+            // Meminta input tahun lahir
+            System.out.print("Masukkan tahun lahir: ");
+            int tahunLahir = scanner.nextInt();
+
+            // Menutup Scanner setelah selesai digunakan
+            scanner.close();
+
+            // Mendapatkan tahun saat ini
+            int tahunSekarang = LocalDate.now().getYear();
+
+            // Menghitung umur
+            int umur = tahunSekarang - tahunLahir;
+
+            // Menentukan jenis kelamin
+            String jenisKelaminStr;
+            if (jenisKelamin == 'L' || jenisKelamin == 'l') {
+                jenisKelaminStr = "Laki-laki";
+            } else if (jenisKelamin == 'P' || jenisKelamin == 'p') {
+                jenisKelaminStr = "Perempuan";
+            } else {
+                jenisKelaminStr = "Tidak diketahui";
+            }
+
+            // Menampilkan hasil
+            System.out.println("\nData Diri:");
+            System.out.println("Nama           : " + nama);
+            System.out.println("Jenis Kelamin  : " + jenisKelaminStr);
+            System.out.println("Umur           : " + umur + " tahun");
         }
-    }
 }
--- Tugas/tugas 1/src/Main.java
+++ Tugas/tugas 1/src/Main.java
@@ -1,15 +1,38 @@
+import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        public static void main (String[] args) {
+            Scanner scr = new Scanner(System.in);
+            String AdminName_Real = "Loka";
+            String AdminPass_Real = "Loka2005";
+            String StudentName_Real = "Loka";
+            String StudentPass_Real = "2005";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            System.out.println("Pilih Login\n1. Admin\n2. Mahasiswa");
+            System.out.print("Masukan Pilihan : ");
+            int pilihan = scr.nextInt();
+            scr.nextLine();
+            if (pilihan == 1) {
+                System.out.print(" Masukan Username : ");
+                String AdminName = scr.nextLine();
+                System.out.print(" Masukan Password : ");
+                String AdminPass = scr.nextLine();
+                if (AdminName.equals(AdminName_Real) && AdminPass.equals(AdminPass_Real)) {
+                    System.out.println("Login Admin Berhasil!");
+                }else System.out.println("Login Gagal. Username Atau Password Salah!");
+
+            } else if (pilihan == 2) {
+                System.out.print(" Masukan Username : ");
+                String StudentName = scr.nextLine();
+                System.out.print(" Masukan NIM : ");
+                String StudentPass = scr.nextLine();
+                if (StudentName.equals(StudentName_Real) && StudentPass.equals(StudentPass_Real)) {
+                    System.out.println("Login Mahasiswa Berhasil!");
+                    System.out.println("Nama : " + StudentName);
+                    System.out.println("NIM : " + StudentPass);
+                }else System.out.println("Login Gagal. Username Atau NIM Salah!");
+
+            }else System.out.println("Pilihan Tidak Valid!");
         }
-    }
 }
